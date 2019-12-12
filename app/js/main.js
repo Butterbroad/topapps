882d69fa146b6dabdbe8d5a7b258e5dfcbab43e8
Butterbroad <pyatochevskiy@gmail.com>
2019-12-12 23:05:32 +0300

    adapted page

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,29 @@ $(function(){
     slidesToScroll: 4,
     arrows: false,
     dots: true,
+    responsive: [
+      {
+        breakpoint: 1120,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 890,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 610,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
   });
 
   $('.feedbacks__slider').slick({
@@ -12,6 +35,19 @@ $(function(){
     slidesToScroll: 2,
     arrows: false,
     dots: true,
+    responsive: [
+      {
+        breakpoint: 1150,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+  
+  $('.nav__burder').on('click',function(){
+    $('.header__nav-list').slideToggle();
   });
 
   $('input[type="file"], select').styler();
